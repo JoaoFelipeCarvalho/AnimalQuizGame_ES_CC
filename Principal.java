37faf0eed3284c0871fdb17e95d9c1ebae24ddc9
João Felipe Carvalho <42190642+JoaoFelipeCarvalho@users.noreply.github.com>
2019-08-30 16:16:52 -0300

    Itens de codigo correto
    Pasta completa pode ser obtida no seguinte link:https://drive.google.com/open?id=1UOrPojEA5TvWKDSd5oCbcQ9GB3iHVPbU

---
+++ Principal.java
@@ -0,0 +1,18 @@
+
+import br.univali.computacao.animais.gui.PlataformaGUI;
+/**
+ * @author 1978233
+ * 
+ */
+
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        PlataformaGUI novoJogo = new PlataformaGUI();
+        novoJogo.jogar();
+        
+    }
+}
